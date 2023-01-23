d6e4a7850845044e7e3c17efbfd22d8cc965bd10
Parm345 <51205078+Parm345@users.noreply.github.com>
2023-01-23 13:06:15 -0500

    arm_simulation.py can publish to /joint_states and move arm in rivz
    - tested with old, hence modification to launch file
    - renamed a joint in old arm urdf

---
+++ rover/arm/scripts/arm_simulation_control.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import rospy
+from sensor_msgs.msg import JointState
+from std_msgs.msg import Header
+
+if __name__ == "__main__":
+    try:
+        rospy.init_node("arm_sim_control") # start node
+        jointPublisher = rospy.Publisher("joint_states", JointState, queue_size=10) # refrence to the output topic, you can have multiple in a script
+
+        rate = rospy.Rate(10) # 10 hz
+        while not rospy.is_shutdown():
+
+            # data to be published
+            newJointState = JointState()
+            newJointState.header = Header()
+            newJointState.header.stamp = rospy.Time.now()
+            newJointState.name = ["Joint_1", "Joint_2", "Joint_3", "Joint_4", "Joint_5", "Joint_6"]
+            newJointState.position = [0, 0, 1, 0, 1, 1] # Angles in radians [Joint_1, Joint_2, ....]
+
+            jointPublisher.publish(newJointState) # send data to be published
+
+            rate.sleep() # controls loop rate based on what is set in the rate variable 
+    except:
+        rospy.loginfo("Some Error Has Occured")
