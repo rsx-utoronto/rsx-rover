57fb30fbb22399a504e48831b0bebb4afdf774e2
rickyzhu2004 <73552739+rickyzhu2004@users.noreply.github.com>
2023-01-28 11:11:56 -0500

    Update arm_simulation_control.py
    Included anglePosition() function to continuously update the arm's position in rviz

--- rover/arm/scripts/arm_simulation_control.py
+++ rover/arm/scripts/arm_simulation_control.py
@@ -9,6 +9,12 @@ if __name__ == "__main__": # if used rosrun on this script then ...
         jointPublisher = rospy.Publisher("joint_states", JointState, queue_size=10) # refrence to the output topic, you can have multiple in a script
 
         rate = rospy.Rate(10) # 10 hz
+
+        def anglePosition():
+            angle1, angle2, angle3, angle4, angle5, angle6 = input("Enter 6 angles in radians: ").split()
+            angleArray = list(map(float, [angle1, angle2, angle3, angle4, angle5, angle6])) # Angles in radians
+            return angleArray
+        
         while not rospy.is_shutdown():
 
             # data to be published
@@ -16,10 +22,8 @@ if __name__ == "__main__": # if used rosrun on this script then ...
             newJointState.header = Header()
             newJointState.header.stamp = rospy.Time.now()
             newJointState.name = ["Joint_1", "Joint_2", "Joint_3", "Joint_4", "Joint_5", "Joint_6"]
-            newJointState.position = [0, 0, 1, 1, 1, 1] # Angles in radians [Joint_1, Joint_2, ....], re-run this script and change the values to see it work.
-
+            newJointState.position = anglePosition() # Angles in radians [Joint_1, Joint_2, ....], re-run this script and change the values to see it work.
             jointPublisher.publish(newJointState) # send data to be published
-
             rate.sleep() # controls loop rate based on what is set in the rate variable 
     except:
         rospy.loginfo("Some Error Has Occured")
