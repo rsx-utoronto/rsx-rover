9ce85398d5ba74bd8618ab18623bd76cf09e46d6
rickyzhu2004 <73552739+rickyzhu2004@users.noreply.github.com>
2023-02-04 11:17:07 -0500

    Update arm_simulation_control.py

--- rover/arm/scripts/arm_simulation_control.py
+++ rover/arm/scripts/arm_simulation_control.py
@@ -4,19 +4,19 @@ import math
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Header
 
+def anglePosition():
+            angle1, angle2, angle3, angle4, angle5, angle6 = input("Enter 6 angles (seperated by spaces) in degrees: ").split() # Angles in degrees
+            angleArray = list(map(float, [angle1, angle2, angle3, angle4, angle5, angle6])) 
+            for i in range(len(angleArray)):
+                angleArray[i] = angleArray[i] * math.pi/180 # Conversion from degrees to radians
+            return angleArray
+
 if __name__ == "__main__": # if used rosrun on this script then ...
     try:
         rospy.init_node("arm_sim_control") # start node
         jointPublisher = rospy.Publisher("joint_states", JointState, queue_size=10) # refrence to the output topic, you can have multiple in a script
 
         rate = rospy.Rate(10) # 10 hz
-
-        def anglePosition():
-            angle1, angle2, angle3, angle4, angle5, angle6 = input("Enter 6 angles (seperated by spaces) in degrees: ").split() # Angles in degrees
-            angleArray = list(map(float, [angle1, angle2, angle3, angle4, angle5, angle6])) 
-            for i in range(len(angleArray)):
-                angleArray[i] = angleArray[i] * math.pi/180 # Conversion from degrees to radians
-            return angleArray
         
         while not rospy.is_shutdown():
             # data to be published
