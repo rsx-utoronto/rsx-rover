9eed04751b9f59c6af37cde02d1f9e78d1813c99
abhay126 <vabhay.12601@gmail.com>
2023-03-11 18:13:32 -0500

    FInal changes after testing servo code with confirmation that it works

--- rover/arm/scripts/test_servo.py
+++ rover/arm/scripts/test_servo.py
@@ -1,8 +1,16 @@
 import arm_servo as servo
+from MapManualJoystick import *
+
+triggered = 0
 
 while 1: 
-    trigger = int(input("1 or 0\n"))
-    if trigger:
+    trigger = MapJoystick(GetManualJoystickFinal.GetManualJoystick(), current_pos, speed_limit, t-time.time())[-1]
+    print(trigger)
+    if trigger and triggered == 0:
+        print("Triggering")
         servo.write_servo_high_angle()
-    else:
+        triggered = 1
+    elif trigger == 0 and triggered == 1:
+        print("Triggering Again")
         servo.write_servo_low_angle()
+        triggered = 0
