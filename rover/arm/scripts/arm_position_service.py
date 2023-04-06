bafe6e1cee191bd9a90d5423c80dc9ed60acbf48
Claylk <116410609+Claylk@users.noreply.github.com>
2023-04-05 20:50:41 -0400

    create arm_position_service.py and arm_positions.srv

---
+++ rover/arm/scripts/arm_position_service.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import rospy
+import arm_master_control as arm_mc
+import sys
+sys.path.append('/home/rsx/rover_ws/src/rsx-rover')
+
+from rover.srv import arm_positions
+
+
+
+
+
+
+
+
+
