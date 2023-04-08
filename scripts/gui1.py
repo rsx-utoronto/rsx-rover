f7aeaf845b7cd3480b4493f790ef73efb56433e9
mavricka <katarina.mavric@mail.utoronto.ca>
2023-04-08 15:21:52 -0400

    Working, need to add customizing label/button

--- scripts/gui1.py
+++ scripts/gui1.py
@@ -9,64 +9,68 @@ import tkinter as tk
 class Label:
     labels = [] # this is a list of the created labels
 
-    def __init__(self, queue, pubType) -> None:
+    def __init__(self, queue, subType) -> None:
         """
         queue is the subscriber queue this label should follow
-        pubType is the data type of the subscriber queue
+        subType is the data type of the subscriber queue
         """
         self.queue = queue
-        self.pubType = pubType
-        self.label = Label(root, text="placeholder")
+        self.pubType = subType
+        self.label = tk.Label(root, text="placeholder")
         self.label.pack()
         Label.labels.append(self)
-    
-    def updateLabels(): 
-        # this updates all the Labels that have been created
-        for label in Label.labels:
-            rospy.Subscriber(label.queue, label.pubType, label.callback)
-        root.after(100, Label.updateLabels())
     
     def callback(self, data):
         # this updates the specific instance Label
         self.label['text'] = data.data
 
-"""
-def updatePublisher(b):
-    global v
-    v = not b
-    pub_gui.publish(v)
-    print('here')
-"""
+def updateLabels(): 
+     # this updates all the Labels that have been created
+    for label in Label.labels:
+        rospy.Subscriber(label.queue, label.pubType, label.callback)
+    root.after(100, lambda: Label.updateLabels())
+
+class Button():
+    def __init__(self, queue, pubType, varChoice1, varChoice2) -> None:
+        """
+        The button publishes to queue with data type pubType and toggles between the list varChoice1/2
+        """
+        self.pub = rospy.Publisher(queue, pubType, queue_size=1)
+        self.var1 = pubType
+        self.var2 = pubType
+        self.curVar = pubType
+        self.var1 = varChoice1
+        self.var2 = varChoice2
+        self.curVar = varChoice1
+        self.btn = tk.Button(root, text = varChoice1, bd = '5', command = lambda: self.updatePublisher())
+        self.btn.pack(side = 'top')   
+        self.pub.publish(self.curVar)
+
+    def updatePublisher(self):
+        if self.curVar == self.var1:
+            self.curVar = self.var2
+        else:
+            self.curVar = self.var1
+        self.pub.publish(self.curVar)
+        self.btn['text'] = self.curVar
 
 def main():
-    #global pub_gui
-    #pub_gui = rospy.Publisher('gui', Bool, queue_size=100)
     rospy.init_node("gui")
     
     # create a tkinter window
     global root
     root = tk.Tk()             
     # Open window having dimension 100x100
-    root.geometry('200x200')
+    root.geometry('300x300')
 
     # create all the labels
-    lab = Label('publish', Int64)
+    Label('publish', Int64)
+    Label('publish2', Int64)
 
-    """
-    global v
-    v = Bool()
-    v = True
-    
-    # Create a Button
-    btn = Button(root, text = 'Click me !', bd = '5',
-                        command = lambda: updatePublisher(v))
-    
-    # Set the position of button on the top of window.  
-    btn.pack(side = 'top')   
-    pub_gui.publish(v)
-    """
+    # create all the buttons
+    Button('gui', Bool, True, False)
 
-    Label.updateLabels()
+    updateLabels()
     root.mainloop()
 
 if __name__ == '__main__':
--- scripts/pub.py
+++ scripts/pub.py
@@ -5,16 +5,21 @@ import time
 from std_msgs.msg import Int64 
 
 def main():
-    pub = rospy.Publisher('publish', Int64, queue_size=1)
     rospy.init_node("publish")
+    pub = rospy.Publisher('publish', Int64, queue_size=1)
+    pub2 = rospy.Publisher('publish2', Int64, queue_size=1)
+    
     count = Int64()
+    count2 = Int64()
     # we continuously send pings to check network communication is working
     counter = 1
     while not rospy.is_shutdown():
         pub.publish(count)
+        pub2.publish(count2)
         print(count)
         counter += 1
         count.data = counter
+        count2.data = counter + 100000
         time.sleep(0.01) # this is the amount of time (in seconds) waiting before checking the connection again
 
 if __name__ == '__main__':
