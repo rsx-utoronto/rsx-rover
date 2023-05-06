f3d0b207c66716402268260ab00ceb47a7df1f9b
kevinsun203 <kevingrandflyer@gmail.com>
2023-05-06 17:21:53 +0000

    Added traversability score calculation.

--- rover/autonomy/scripts/point_cloud_subscriber.cpp
+++ rover/autonomy/scripts/point_cloud_subscriber.cpp
@@ -1,6 +1,7 @@
 /* Subscribes to lidar to receive point cloud data */
 
 #include "ros/ros.h"
+#include "traversability_score_helpers.h"
 
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
@@ -10,37 +11,25 @@
 #include <sensor_msgs/PointCloud2.h>
 
 void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg) {
-    // Container for original & filtered data
-    pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2;
-    pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
-    pcl::PCLPointCloud2 cloud_filtered;
+    // Convert the sensor_msgs::PointCloud2 message to pcl::PointCloud
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromROSMsg(*cloud_msg, *cloud);
 
-    // Convert to PCL data type
-    pcl_conversions::toPCL(*cloud_msg, *cloud);
+    /* segment point cloud here */
 
-    // Perform the actual filtering
-    pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
-    sor.setInputCloud (cloudPtr);
-    sor.setLeafSize (0.1, 0.1, 0.1);
-    sor.filter (cloud_filtered);
-
-    // Convert to ROS data type
-    sensor_msgs::PointCloud2 output;
-    pcl_conversions::moveFromPCL(cloud_filtered, output);
+    // calculate the traversability score
+    double score = calculateTraversabilityScore(cloud);
+    std::cout << "traversability score: " << score << std::endl;
 }
 
 int main(int argc, char** argv) {
-    // Initialize the ROS Node
-    ros::init (argc, argv, "lidar_point_cloud");
+    // Initialize the ROS Node with a node handler
+    ros::init (argc, argv, "lidar_pcl_subscriber");
     ros::NodeHandle nh;
 
-    // Print "Hello" message with node name to the terminal and ROS log file
-    ROS_INFO_STREAM("Preparing to receive point cloud " << ros::this_node::getName());
+    // Create a ROS Subscriber to lidar with a queue_size of 1 and a callback function to cloud_cb
+    ros::Subscriber sub = nh.subscribe("Lidar topic name?", 1, cloud_cb);
 
-    // Create a ROS Subscriber to IMAGE_TOPIC with a queue_size of 1 and a callback function to cloud_cb
-    ros::Subscriber sub = nh.subscribe("Lidar topic name????", 1, cloud_cb);
-
-    // Spin
     ros::spin();
 
     // Success
