e5604809a2e605ca8ff6d484d4bef6099e70ce8b
Abhay Verma <abhay.verma@mail.utoronto.ca>
2023-06-17 13:44:19 -0400

    Updated the setup shell script for canbus network

--- scripts/utils/gen/setup_can.sh
+++ scripts/utils/gen/setup_can.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
-
-sudo ip link set can0 type can bitrate 1000000 
+#echo "Adding CAN network"
+#sudo ip link add dev can0 type can
+echo "Setting CAN network bitrate and queue length"
+sudo ip link set can0 type can bitrate 1000000
+sudo ifconfig can0 txqueuelen 1000
+echo "Enabling the CAN network"
+sudo ip link set down can0
 sudo ip link set up can0
