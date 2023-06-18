6c526f3f768b669816db7dfc66698fbfd59dfccb
Garvishb <togarvish@gmail.com>
2023-06-18 17:22:07 -0400

    Fixed -nan problem in map_saver

--- rover/autonomy/scripts/map_saver.sh
+++ rover/autonomy/scripts/map_saver.sh
@@ -5,6 +5,7 @@ sleep 5
 while true; do
 
     rosrun map_server map_saver -f ~/rsx_ws/src/rsx-rover/rover/autonomy/maps/current_map map:=/ransac_grid
+    # rosrun map_server map_server ~/rsx_ws/src/rsx-rover/rover/autonomy/maps/current_map.yaml frame_id:=map /map:=/cur_oc /map_metadata:=cur_oc_metadata
     echo done!
     sleep 5
 
