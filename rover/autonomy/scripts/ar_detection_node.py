efc385784c8933fff240f63a32dcf3f66d2a0d87
Catherine Glossop <catherine.glossop@mail.utoronto.ca>
2023-07-01 01:18:04 -0400

    state machine in progress

--- rover/autonomy/scripts/ar_detection_node.py
+++ rover/autonomy/scripts/ar_detection_node.py
@@ -3,44 +3,104 @@
 import rospy
 import cv2
 import cv2.aruco as aruco
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge, CvBridgeError
+from rsx_rover.msg import GPSMsg, StateMsg
 import numpy as np
 import os
 
 bridge = CvBridge()
 
-def image_callback(ros_image):
-    global bridge
-    try:
-        cv_image = bridge.imgmsg_to_cv2(ros_image,"bgr8")
-    except CvBridgeError as e:
-        print(e)
-    else:
-        findArucoMarkers(cv_image)
+class ARucoTagDetectionNode():
 
-# from CIRC rules, 4*4_50
-# https://circ.cstag.ca/2022/rules/#autonomy-guidelines:~:text=All%20ArUco%20markers%20will%20be%20from%20the%204x4_50%20dictionary.%20They%20range%20from%20marker%200%20to%2049.
+    def __init__(self):
+        self.image_topic = "/camera/color/image_raw"
+        self.info_topic = "/camera/color/camera_info"
+        self.image_sub = rospy.Subscriber(self.image_topic, Image, self.image_callback)
+        self.cam_info_sub = rospy.Subscriber(self.info_topic, CameraInfo, self.info_callback)
+        self.state_sub = rospy.Subscriber(StateMsg, self.state_callback)
+        self.aruco_pub = rospy.Publisher('rover_state', StateMsg, queue_size=10)
+        self.vis_pub = rospy.Publisher('vis/current_aruco_detections', Image, queue_size=10)
+        self.bridge = CvBridge()
+        self.current_state = -1
+        self.curr_aruco_detections = {}
+        self.detected_aruco_ids = []
+        self.detect_thresh = 5
+        self.permanent_thresh = 10
+        self.K = None
+        self.D = None
 
-def findArucoMarkers(img, markerSize=4, totalMarkers=50, draw=True):
-    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    key =getattr(aruco, 'DICT_' + str(markerSize) + 'X' + str(markerSize) + str(totalMarkers))
-    arucoDict = aruco.Dictionary_get(key)
-    arucoParam = aruco.DetectorParameters_create()
-    bboxs, ids,rejected=aruco.detectMarkers(imgGray,arucoDict,parameters=arucoParam)
+    def image_callback(self, ros_image):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(ros_image,"bgr8")
+        except CvBridgeError as e:
+            print(e)
+        else:
+            # Do we need to undistort?
+            self.findArucoMarkers(cv_image)
+    
+    def info_callback(self, info_msg):
 
-    print(ids)
-    if draw:
-        aruco.drawDetectedMarkers(img,bboxs)
+        self.D = np.array(info_msg.D)
+        self.K = np.array(info_msg.K)
+        self.K = self.K.reshape(3,3)
+
+
+    def state_callback(self, state_msg):
+        
+        self.current_state = state_msg
+
+    # from CIRC rules, 4*4_50
+    # https://circ.cstag.ca/2022/rules/#autonomy-guidelines:~:text=All%20ArUco%20markers%20will%20be%20from%20the%204x4_50%20dictionary.%20They%20range%20from%20marker%200%20to%2049.
+
+    def findArucoMarkers(self, img, markerSize=4, totalMarkers=50, draw=True):
+        imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        key = getattr(aruco, 'DICT_' + str(markerSize) + 'X' + str(markerSize) + str(totalMarkers))
+        arucoDict = aruco.Dictionary_get(key)
+        arucoParam = aruco.DetectorParameters_create()
+        bboxs, ids, rejected = aruco.detectMarkers(imgGray,arucoDict,parameters=arucoParam)
+
+        for id in ids: 
+
+            if id in self.curr_aruco_detections: 
+                self.curr_aruco_detections[id] += 1
+                if self.curr_aruco_detections[id] > self.permanent_thresh:
+                    self.detected_aruco_ids.append(id)
+            
+            else:
+                self.curr_aruco_detections[id] = 1
+        
+        dists_to_tags = []
+        for i, id in enumerate(ids):
+
+            rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(bboxs[i], 0.02, self.K,
+                                                                                self.D)
+            
+            dist = np.linalg.norm(tvec)
+            dists_to_tags.append(dist)
+
+        best_detection = ids[np.argmax(np.array(dists_to_tags))]
+        rospy.loginfo(f"An AR tag was detected with the ID {best_detection}")
+        
+        if draw:
+            aruco.drawDetectedMarkers(img,bboxs)
+        
+        self.vis_pub(img)
+        
+        updated_state_msg = self.current_state
+
+        updated_state_msg.detected_AR_IDs = self.detected_aruco_ids
+        updated_state_msg.curr_AR_IDs = self.curr_aruco_detections.keys()
+        updated_state_msg.best_curr_AR_ID = best_detection
+
+        self.aruco_pub(updated_state_msg)
 
 
 def main():
-    rospy.init_node('image_converter',anonymous=True)
-    # set to the right camera for now, can also set left camera
-    image_topic = "/zed/zed_node/right_raw/image_raw_color"
-    rospy.Subscriber(image_topic, Image, image_callback)
+    rospy.init_node('aruco_tag_detector',anonymous=True)
 
-    # Spin until ctrl + c
+    AR_detector = ARucoTagDetectionNode()
+
     rospy.spin()
 
     ### this code is for webcam 
---
+++ rover/autonomy/scripts/smach_node.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python3
+
+import rospy
+import smach 
+import numpy as np
+from smach_ros import SimpleActionState
+from rsx_rover.msg import GPSMsg, StateMsg
+from std_srvs.srv import Empty, EmptyResponse
+import argparse
+
+class StateMachineNode():
+
+    def __init__(self):
+
+        self.state_subscriber = rospy.Subscriber(StateMsg, self.state_callback)
+        self.autonomy_srv = rospy.Service('rover/launch', Empty, self.launch_callback)
+        self.STATE = "IDLE"
+        self.GPS_GOALS = np.zero
+
+        # State Machine States
+        self.rover_idle = self.RoverIdle()
+        self.rover_initialize = self.RoverInitialize()
+        self.rover_start = self.RoverStart()
+        self.rover_gps_traverse = self.RoverGPSTraverse()
+        self.rover_aruco_traverse = self.RoverARucoTraverse()
+
+    def state_callback(self, state_msg):
+
+        self.GPS_GOAL_REACHED = state_msg.GPS_GOAL_REACHED 
+
+    def launch_callback(self, request):
+        self.handle_launch()
+        return EmptyResponse()
+    
+    def handle_launch(self):
+        self.STATE = "INITIALIZE"
+        print('Autonomy task start detected.')
+
+class RoverIdle(smach.State):
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state IDLE')
+
+        # Your state execution goes here
+        if self.STATE == "IDLE":
+            return 'STAY'
+        elif self.STATE == "INITIALIZE":
+            return 'MOVE_TO_INTIALIZE'
+
+class RoverInitialize(smach.State):
+
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['outcome1','outcome2'])
+        self.counter = 0
+        # Your state initialization goes here
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state FOO')
+        # Your state execution goes here
+        if :
+            return 'outcome1'
+        else:
+            return 'outcome2'
+
+class RoverStart(smach.State):
+
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['outcome1','outcome2'])
+        self.counter = 0
+        # Your state initialization goes here
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state FOO')
+        # Your state execution goes here
+        if xxxx:
+            return 'outcome1'
+        else:
+            return 'outcome2'
+    
+class RoverGPSTraverse(smach.State):
+
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['outcome1','outcome2'])
+        self.counter = 0
+        # Your state initialization goes here
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state FOO')
+        # Your state execution goes here
+        if xxxx:
+            return 'outcome1'
+        else:
+            return 'outcome2'
+
+class RoverARucoTraverse(smach.State):
+
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['outcome1','outcome2'])
+        self.counter = 0
+        # Your state initialization goes here
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state FOO')
+        # Your state execution goes here
+        if xxxx:
+            return 'outcome1'
+        else:
+            return 'outcome2'
+
+class RoverGPSCheckpoint(smach.State):
+
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['outcome1','outcome2'])
+        self.counter = 0
+        # Your state initialization goes here
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state FOO')
+        # Your state execution goes here
+        if xxxx:
+            return 'outcome1'
+        else:
+            return 'outcome2'
+    
+# class RoverScan(smach.State):
+
+#     def __init__(self):
+#         smach.State.__init__(self, outcomes=['outcome1','outcome2'])
+#         self.counter = 0
+#         # Your state initialization goes here
+
+#     def execute(self, userdata):
+#         rospy.loginfo('Executing state FOO')
+#         # Your state execution goes here
+#         if xxxx:
+#             return 'outcome1'
+#         else:
+#             return 'outcome2'
+
+# class RoverTimeout(smach.State):
+
+#     def __init__(self):
+#         smach.State.__init__(self, outcomes=['outcome1','outcome2'])
+#         self.counter = 0
+#         # Your state initialization goes here
+
+#     def execute(self, userdata):
+#         rospy.loginfo('Executing state FOO')
+#         # Your state execution goes here
+#         if xxxx:
+#             return 'outcome1'
+#         else:
+#             return 'outcome2'
+
+# class RoverMissionOver(smach.State):
+
+#     def __init__(self):
+#         smach.State.__init__(self, outcomes=['outcome1','outcome2'])
+#         self.counter = 0
+#         # Your state initialization goes here
+
+#     def execute(self, userdata):
+#         rospy.loginfo('Executing state FOO')
+#         # Your state execution goes here
+#         if xxxx:
+#             return 'outcome1'
+#         else:
+#             return 'outcome2'
+
+def main():
+    rospy.init_node('rsx_rover_state_machine')
+
+    state_machine_node = StateMachineNode()
+
+    # Create a SMACH state machine
+    sm = smach.StateMachine(outcomes=['MISSION_OVER', 'MISSION_ABORTED'])
+
+    # Open the container
+    with sm:
+        # Add states to the container
+        smach.StateMachine.add('RoverIdle', RoverIdle(),
+                               transitions={'STAY': RoverIdle(),
+                                            'MOVE_TO_INITIALIZE' : 'RoverInitialize'})
+        smach.StateMachine.add('RoverInitialize', RoverInitialize(), 
+                               transitions={'LOADING' : 'RoverInitialize',
+                                            'LOAD_COMPLETE' : 'RoverStart'})
+        smach.StateMachine.add('RoverStart', RoverStart(), 
+                               transitions={'GPS_GOAL_QUEUED':'RoverGPSTraverse'})
+        smach.StateMachine.add('RoverGPSTraverse', RoverGPSTraverse(), 
+                               transitions={'GPS_GOAL_REACHED':'RoverARucoTraverse'})
+        smach.StateMachine.add('RoverGPSCheckpoint', RoverGPSCheckpoint())
+        smach.StateMachine.add('RoverARucoTraverse', RoverARucoTraverse(),
+                               transitions={'ARUCO_FOUND':'RoverTransition', 
+                                            'SEARCHING': 'RoverScan'})
+
+    # Execute SMACH plan
+    outcome = sm.execute()
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Import command line arguments for task')
+    parser.add_argument('task_config', metavar='f', default="~/rover_ws/src/rsx-rover/task_config.json",
+                        help='Task configuration file')
+
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
+    main()
