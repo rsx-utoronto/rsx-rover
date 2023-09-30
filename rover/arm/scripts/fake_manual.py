1b2652e8e94e017df518ca55409767f8c7e18955
Parm345 <51205078+Parm345@users.noreply.github.com>
2023-09-30 17:46:11 -0400

    Added keyboard feature for fake manual mode

--- rover/arm/scripts/fake_manual.py
+++ rover/arm/scripts/fake_manual.py
@@ -4,18 +4,27 @@ from std_msgs.msg import String, Float32MultiArray
 from rover.msg import ArmInputs
 
 '''
-This code is just to help debug IK messages with 
+This code is just to help debug arm control code when
+the real arm isn't plugged in an manual is needed
 '''
 
 global curStates
 global jointPublisher
-global armAngles
+global armAngles # the angles that fake_manual thinks it's at
+
+CONTROLLER_ENABLED = False
 
 def updateStates(data):
+    ''' Callback function for the /arm_states topic'''
     global curState
     curState = data.data
 
 def updateController(data):
+    ''' Callback function for /arm_inputs 
+
+        Recieves the ArmInput ros message and uses the values to change
+        the arm angles that fake manual thinks it's at.
+    '''
     global jointPublisher
     global armAngles
     global curState
@@ -35,8 +44,12 @@ def updateController(data):
         print(armAngles)
 
 def updateRealAngles(data):
+    ''' Callback function for /arm_goal_pos topic
+
+    Idealized version of manual with no saftey
+    '''
     global curState
-    if curState == "IK":
+    if curState != "Manual":
         global armAngles
 
         tempList = list(data.data)
@@ -50,6 +63,23 @@ def updateRealAngles(data):
         anglesToPublish.data = armAngles
         jointPublisher.publish(anglesToPublish)
 
+def keyboardController(data):
+    ''' Callback function for /keyboard_fake_man topic
+
+    Used to pretend to send inputs to fake manual when 
+    there isn't a controller. Useful in docker debugging.
+    '''
+    global curState
+
+    if curState == "Manual":
+        newAngles = list(data.data)
+        armAngles = newAngles
+
+        anglesToPublish = Float32MultiArray()
+        anglesToPublish.data = armAngles
+        jointPublisher.publish(anglesToPublish)
+
+
 if __name__ == "__main__":
     try:
         rospy.init_node("fake_manual")
@@ -57,8 +87,12 @@ if __name__ == "__main__":
 
         jointPublisher = rospy.Publisher("arm_curr_pos", Float32MultiArray, queue_size=10)
         rospy.Subscriber("arm_state", String, updateStates)
-        rospy.Subscriber("arm_inputs", ArmInputs, updateController)
         rospy.Subscriber("arm_goal_pos", Float32MultiArray, updateRealAngles)
+
+        if CONTROLLER_ENABLED:
+            rospy.Subscriber("arm_inputs", ArmInputs, updateController)
+        else:
+            rospy.Subscriber("keyboard_fake_man", Float32MultiArray, keyboardController)
 
         rospy.spin()
     except Exception as ex:
