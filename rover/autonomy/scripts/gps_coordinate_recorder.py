283cbfc8f773af5ec0182f628636da4c0b96c99a
Liu-Fayi <liufy520@hotmail.com>
2024-02-23 01:23:57 -0500

    add gps coord recorder

---
+++ rover/autonomy/scripts/gps_coordinate_recorder.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import rospy
+from sensor_msgs.msg import NavSatFix
+
+class GPSRecorder:
+    def __init__(self):
+        self.gps_sub = rospy.Subscriber('/gps/fix', NavSatFix, self.callback)
+        self.gps_data_file = open('gps_data.txt', 'w')
+
+    def callback(self, msg):
+        self.gps_data_file.write(str(msg.latitude) + ', ' + str(msg.longitude) + '\n')
+
+if __name__ == '__main__':
+    rospy.init_node('gps_recorder')
+    gps_recorder = GPSRecorder()
+    rospy.spin()
+    gps_recorder.gps_data_file.close()
+    
