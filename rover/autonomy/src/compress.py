549a0cb98c1351eb8962adaa87d2958cf83e2bd9
abhay126 <vabhay.12601@gmail.com>
2024-05-07 21:50:30 -0400

    working compression

--- rover/autonomy/src/compress.py
+++ rover/autonomy/src/compress.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 import rospy
 from sensor_msgs.msg import Image  # Import Image message type from sensor_msgs package
@@ -10,7 +10,7 @@ import cv2
 
 QUALITY = 1
 PUBLISH_RATE = 15
-INPUT_TOPIC = '/zed/zed_node/rgb/image_rect_color'
+INPUT_TOPIC = '/camera/color/image_raw'
 OUTPUT_TOPIC = 'c_stream'
 
 class compressedImage():
@@ -19,7 +19,6 @@ class compressedImage():
         self.inStream = rospy.Subscriber(INPUT_TOPIC, Image, self.callback, queue_size = 1)
         self.outStream = rospy.Publisher(OUTPUT_TOPIC, Image, queue_size=1)
         self.bridge = CvBridge()
-        self.rate = rospy.Rate(PUBLISH_RATE)
 
     def callback(self, data):
         raw = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
@@ -31,9 +30,9 @@ class compressedImage():
 
         self.outStream.publish(compressed_msg)
 
-        self.rate.sleep()
-
 if __name__ == '__main__':
     rospy.init_node('compressnode', anonymous=True)
     compressedImage()
-    rospy.spin()
+    rate = rospy.Rate(PUBLISH_RATE)
+    while not rospy.is_shutdown():
+        rate.sleep()
