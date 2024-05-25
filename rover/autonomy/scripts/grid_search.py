ab57248c4bd02a037639aef83148a696081d81ea
reimyousef <reim.yousef@gmail.com>
2024-05-24 20:55:04 -0400

    ..

--- rover/autonomy/scripts/grid_search.py
+++ rover/autonomy/scripts/grid_search.py
@@ -10,7 +10,7 @@ from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from std_msgs.msg import Float32
 import numpy as np
 
-class grid_search():
+class grid_search_class():
     def __init__(self):
         self.x = 0.0
         self.y = 0.0
@@ -23,7 +23,7 @@ class grid_search():
         self.roll=euler_from_quaternion([0])
         self.pitch=euler_from_quaternion([1])
         self.yaw=euler_from_quaternion([2])
-        
+        self.go_to_loc = False
 
     def newOdom(self, msg):
         # global x, y, theta, init
@@ -41,25 +41,29 @@ class grid_search():
         rospy.init_node("speed_controller")
 
     
-    def follow_path(self, path_list, scale_factor):
+    def follow_path(self, path_list, scale_factor, detector):
         r = rospy.Rate(10)
 
         # path_list = [(0+x,0+y), (3.5+x,0.0+y), (3.5+x, 3.5+y), (-3.5+x, 3.5+y)]
         #             # ,(-3.5+x, -7.0+y), (10.5+x, -7.0+y, (10.5+x,10.5+y), (-10.5+x,10.5+y), 
         #             # (-10.5+x, -14.0+y), (17.5+x, -14.0+y), (17.5+x, 17.5+y), (-17.5+x, 17.5+y), (-17.5+x, -21.0+y), (17.5+x, -21.0+y)]
         point_index = 0  # instead of deleting stuff from a list (which is anyway bug prone) we'll just iterate through it using index variable.
-        # scale_factor = 0.75
         goal = Point ()
-        while not rospy.is_shutdown():
+        while not rospy.is_shutdown() and not detector.isfound():
             if point_index < len(path_list): # so we won't get an error of trying to reach non-existant index of a list
-                goal.x = path_list[point_index][0]  # x coordinate for goal
-                goal.y = path_list[point_index][1]  # y coordinate for goal
-            else:
-                self.stop()  
-            inc_x = (goal.x - x)*scale_factor
-            inc_y = (goal.y - y)*scale_factor 
+                goal.x = path_list[point_index][0] + self.x  # x coordinate for goal
+                goal.y = path_list[point_index][1] + self.y  # y coordinate for goal
+             else:
+                speed.linear.x = 0.0
+                speed.angular.z = 0.0
+               
+                self.nothing_found_at_end()
+                 # I guess we're done?
+                
+            inc_x = (goal.x - self.x)*scale_factor
+            inc_y = (goal.y - self.y)*scale_factor 
 
-            if (x==0 or y==0):
+            if (self.x==0 or self.y==0):
                 angle_to_goal = math.pi/2
             else:
                 angle_to_goal = math.atan2 (inc_y, inc_x) # this is our "bearing to goal" as I can guess
@@ -79,12 +83,20 @@ class grid_search():
                     self.speed.angular.z = 0.0
                     self.pub.publish(self.speed)
             else:
-                point_index += 1 
-        
-        r.sleep()
+                point_index += 1
+            r.sleep()
+        self.stop()
              
     def stop(self):
         self.speed.linear.x = 0.0
         self.speed.angular.z = 0.0
+        self.go_to_loc = True
+        
+    def nothing_found_at_end(self):
+         print("nothing found at end")
+        
+        
     
+    def is_go_to_loc (self):
+        return self.go_to_loc
         
--- rover/autonomy/scripts/main_urc.py
+++ rover/autonomy/scripts/main_urc.py
@@ -21,24 +21,67 @@ from std_srvs.srv import Empty, EmptyResponse
 import tf2_ros
 import os
 import aruco_homing
+from aruco_homing import Aimer
 import ar_detection_node 
-import grid_aruco_search 
+from ar_detection_node import ARucoTagDetectionNode
+import grid_search 
+from grid_search import grid_search_class
 
 class maincaller(object):
     def __init__(self, name, gps_type):
         self.name = name
-        self.gps_type = gps_type
-        self.sub_ar_detection_node= ar_detection_node.
+        self.gps_type = gps_type # array
+        self.scale_factor = 0.75
+        # self.sub_ar_detection_node = ar_detection_node.aruco_pub
+        # self.sub_ar_homing = aruco_homing.
+        # self.grid_aruco_search  =
         
-        
-        # want to have these three args. gps, aruco, object
+    def next(self):
+        if self.name == "aruco":
+            self.aruco()
+        elif self.name == "gps":
+            self.gps()
+        elif self.name == "object":
+            self.object()        
     
-    
+    def aruco (self):
+        path_list = [(0,0), (3.5,0.0), (3.5, 3.5), (-3.5, 3.5)]
+        #             # ,(-3.5, -7.0), (10.5, -7.0), (10.5,10.5), (-10.5,10.5), 
+        #             # (-10.5, -14.0), (17.5, -14.0), (17.5, 17.5), (-17.5, 17.5), (-17.5, -21.0), (17.5, -21.0)]
         
+        # jack's detection begins 
+        #if nothing is initially detected, start grid search
+        # if something is detected, shit from jack's pass
+        # if nothing is detected, stop everything and give up on task.
         
+        tag_detector = ARucoTagDetectionNode()
+        grid_s=grid_search_class()  
+        if tag_detector.is_found() == False:      
+            grid_s.follow_path(self, path_list, self.scale_factor, tag_detector)
+        if grid_s.is_go_to_loc == True: 
+            
+            
+        else:
+            print("nothign found")
+            
         
+            
+       
+            
+            
 
+
+        
+    
+    def gps(self):
+        pass
+    
+    def object (self):
+        pass        
+        
+    
 if __name__ == "__main__":
-    gps=maincaller()
+    ar=maincaller(aruco, gps..)
+    
 
     
