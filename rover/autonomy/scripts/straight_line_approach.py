fe1019c3fadb6f6a4db321554aeec852c3649362
aoran-jiao <jiao.aoran2001@gmail.com>
2024-06-29 17:50:27 -0400

    STRAIGHT LINE WORKS!!!!

--- rover/autonomy/scripts/straight_line_approach.py
+++ rover/autonomy/scripts/straight_line_approach.py
@@ -28,9 +28,11 @@ def ToEulerAngles(w, x, y, z):
 
 def odom_callback(msg):
     global x, y, heading
+    
     x = msg.pose.pose.position.x
     y = msg.pose.pose.position.y
     heading = ToEulerAngles(msg.pose.pose.orientation.w, msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z)[2]
+    print("XXX",x,y)
     # heading is in radians
 
 def target_callback(msg):
@@ -39,42 +41,82 @@ def target_callback(msg):
     target_y = msg.data[1]
 
 def straight_line_approach(lin_vel, ang_vel):
-    rospy.Subscriber('odom', Odometry, callback=odom_callback) # change topic name
+    rospy.Subscriber('/rtabmap/odom', Odometry, callback=odom_callback) # change topic name
     rospy.Subscriber('target', Float64MultiArray, callback=target_callback) # change topic name
     # float64[2] data format: [x, y]
     pub = rospy.Publisher('drive', Twist, queue_size=10) # change topic name
     rospy.init_node('straight_line_approach')
     rate = rospy.Rate(10)
+    print("till here works")
     while not rospy.is_shutdown():
         msg = Twist()
         if target_x == None or target_y == None or x == None or y == None:
-            continue
-        target_heading = math.atan2(target_x - x, target_y - y) # in radians
+            continue      
+        target_heading = math.atan2(target_y - y, target_x - x) # in radians #1. Switch places, 
+        print("target heading", target_heading)  
         target_distance = math.sqrt((target_x - x) ** 2 + (target_y - y) ** 2)
+        
+
+
+        print("target_distance", target_distance)
+        print(x,y)
         angle_diff = target_heading - heading
-        if angle_diff > math.pi:
+        print("Angle dist:", angle_diff)
+        if angle_diff > math.pi: #this makes sure angle is in [-pi, pi] 
             angle_diff -= 2 * math.pi
         elif angle_diff < -math.pi:
             angle_diff += 2 * math.pi
-        if abs(angle_diff) < 0.1:
-            msg.linear.x = lin_vel
+
+        if target_distance <0.05:
+            msg.linear.x=0
+            msg.angular.z=0
+            print("stoping")
+            pub.publish(msg)
+            # next = True
+            break
+
+
+        if abs(angle_diff) <= 0.2: #changed this ti < and =
+            delta=target_x-x
+            print("target_reached_x",delta)
+            print("current_X", x)
+            #if delta is negative, go to negative velocity.
+            msg.linear.x =lin_vel
+            
+          
+            #can delete this part and just do msg.linear.x =0 
+            # if delta>0.5:
+            #     print("target still greater than 0 so moving forward")
+            #     msg.linear.x = lin_vel
+            # elif delta<0.5:
+            #     msg.linear.x = -lin_vel
+            #     print("going in negative velocity now", -lin_vel)
+            # else: 
+            #     print("target is iwthin 0.5") 
+            #     msg.linear.x = 0
+        
             msg.angular.z = 0
-        elif angle_diff > 0:
+            
+        elif angle_diff > 0.2: #changes this from 0 to 0.3
             msg.linear.x = 0
+            print("this is 1")
             msg.angular.z = ang_vel
         else:
             msg.linear.x = 0
+            print('this is 2')
             msg.angular.z = -ang_vel
+            
         pub.publish(msg)
         rate.sleep()
 
+
 if __name__ == '__main__':
-    x = None
-    y = None
-    heading = None
-    target_x = None
-    target_y = None
+    x = 0
+    y = 0
+    heading = 0
+    target_x = 15
+    target_y = -6
     try:
-        straight_line_approach(1, 1) # change linear and angular velocities
+        straight_line_approach(1.8, 0.5) # change linear and angular velocities
     except rospy.ROSInterruptException:
-        pass
+        pass  
