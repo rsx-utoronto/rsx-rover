139245d0ac64c9b0c2b96065773c8795bd016301
Garvishb <togarvish@gmail.com>
2024-07-02 00:11:42 -0400

    New drive algorithm, currently acceleration working, need testing

---
+++ rover/drive/README.md
@@ -0,0 +1,38 @@
+# Manual Control Rover
+
+The drive sender node is used to send velocity commands to the `/drive` topic
+
+## Currently:
+
+- The left joystick controls the linear and angular velocity
+    - the y vector controls the linear velocity
+    - the x vector controls the angular velocity
+- R1 to increase the gear
+- L1 to decrease the gear
+- PS button to stop the rover
+
+## Goal:
+
+- R2 to accelerate, L2 to decelarate 
+	- If both, move at that constant speed
+    - If none, stop the rover with max decelearation
+- Triangle to increase gear, square to decrease gear
+- Left joystick to control steering
+	- the y vector is not used
+	- the x vector is used to: 
+		- control steering (more x, more steering)
+		- control lnear speed (more x, less lin speed)
+- Circle to turn completely on spot (linear speed = 0)
+- Reimplement the network status thing so that rover stops moving as soon as we get disconnected from the network
+
+### Notes:
+
+- Removed the *5 from max speed (which was for the gear ratio) because I observed that rover reached max speed at 2.5
+- Changed the robot radius to 1 (it was 0.8 before) which is accurate for current iteration of the rover
+- Set maximum acceleration to 0.075 - can be changed based on testing
+- There's a problem: the code only does stuff when the controller is getting input (changing things on the controller) however, when the controller state is constant (like there is button pressed or R2 is pressed completely), joy topic doesn't get any input and the algorithm doesn't run
+    - To fix this, the algorithm needs to be outside the joy callback function
+    - Done
+
+
+
--- rover/drive/src/drive_sender_node.cpp
+++ rover/drive/src/drive_sender_node.cpp
@@ -12,21 +12,39 @@
 #include <std_msgs/Bool.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
+#include <cmath>
+
+/*
+- R2 to accelerate, L2 to decelarate 
+	- If both, move at that constant speed
+- Triangle to increase gear, square to decrease gear
+- Left joystick to control steering
+	- the y vector is not used
+	- the x vector is used to: 
+		- control steering (more x, more steering)
+		- control lnear speed (more x, less lin speed)
+- Circle to turn completely on spot (linear speed = 0)
+*/
 
 class TeleopRover
 {
 public:
-	TeleopRover();
+	TeleopRover(); // constructor (just like __init__ in python) // has to be the same name as the class
 	void publishDrive();
 	void pubConstSpeed();
 	void joyCallback(const sensor_msgs::Joy::ConstPtr &joy);
 	void networkCallback(const std_msgs::Bool::ConstPtr& net_stat);
 	void stateCallback(const rover::StateMsg::ConstPtr& state);
+	void SetVelocity();
 
 	ros::NodeHandle nh;
-	double robot_radius = 0.8;
-	double MAX_LINEAR_SPEED = 2.5*5; // 5 times because of gear ratio // 2.5 speed est * 0.65 from rough calibration
+	double robot_radius = 1;
+	double MAX_LINEAR_SPEED = 2.5; // 2.5 speed est * 0.65 from rough calibration
 	double MAX_ANGULAR_SPEED = MAX_LINEAR_SPEED*robot_radius;
+	double MAX_ACCELERATION = 0.075;
+	double lin_vel = 0;
+	double prev_lin_vel = 0;
+	// double TIME = 
 	double gear = 0; // set to 0 initially
 	ros::Publisher drive_pub;
 	ros::Subscriber joy_sub;
@@ -37,6 +55,11 @@ public:
 	bool MANUAL_ENABLED = true;
 	bool KILL_PRESSED = false;
 	bool gear_pressed = false;
+	// Initialize buttons and axes with default values
+	// this gets run every time the loop in SetVelocity runs
+	// This is fine in general except when we need to remember the previous value of the buttons (like in the case of KILL)
+	std::vector<int> buttons = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	std::vector<float> axes = {-0.0, -0.0, 1.0, -0.0, -0.0, 1.0, -0.0, -0.0};
 };
 
 TeleopRover::TeleopRover()
@@ -45,100 +68,15 @@ TeleopRover::TeleopRover()
 	TeleopRover::joy_sub = nh.subscribe("/software/joy", 10, &TeleopRover::joyCallback, this);
 	TeleopRover::net_sub = nh.subscribe("/network_status", 1, &TeleopRover::networkCallback, this);
 	TeleopRover::state_sub = nh.subscribe("/rover_state", 1, &TeleopRover::stateCallback, this);
-	network_status = false;
+	// network_status = false;
 }
 
 void TeleopRover::joyCallback(const sensor_msgs::Joy::ConstPtr &joy)
 {
-	int KILL = 10;
-	if (joy->buttons[KILL] == 1){
-		if (KILL_PRESSED == false) {
-			ROS_INFO("KILL ENGAGED");
-			twist.linear.x = 0;
-			twist.linear.y = 0;
-			twist.linear.z = 0;
-			twist.angular.x = 0;
-			twist.angular.y = 0;
-			twist.angular.z = 0;
-			drive_pub.publish(twist);	
-			KILL_PRESSED = true;
-		} else {
-			ROS_INFO("KILL DISENGAGED");
-			KILL_PRESSED = false;
-		}
-	}
-
-	if (MANUAL_ENABLED && ~KILL_PRESSED){
-		twist.linear.x = 0; 
-		twist.linear.y = 0;
-		twist.linear.z = 0;
-		twist.angular.x = 0;
-		twist.angular.y = 0;
-		twist.angular.z = 0;
-		// indexs for controller values
-		int R2 = 5;
-		int L2 = 2;
-		int LS_x = 0; // x axis of joystick
-		int LS_y = 1; // y axis of joystick
-		int dec_speed = 4;
-		int inc_speed = 5;
-
-		// Values from Controller
-		double posThrottle = joy->axes[R2];
-		double negThrottle = joy->axes[L2];
-		double turnFactor_x = static_cast<double>(joy->axes[LS_x]);
-		double turnFactor_y = static_cast<double>(joy->axes[LS_y]);
-		double lin_vel;
-
-		// Encoding value from joystick 
-
-		if (turnFactor_y != 0) {
-			lin_vel = 255.0 * turnFactor_y;
-		} else {
-			lin_vel = 0;
-		}
-
-		if (gear_pressed)
-		{
-			if (joy->buttons[dec_speed] != 1 && joy->buttons[inc_speed] != 1)
-			{
-				gear_pressed = false;
-			}
-		}
-		else
-		{
-			// Encoding values for gear selection (range 0 - 1)
-			if (joy->buttons[dec_speed] == 1) // reduce
-			{
-				if (gear >= 0.03)
-					gear -= 0.03;
-				else
-					gear = 0;
-				gear_pressed = true;
-			}
-			else if (joy->buttons[inc_speed] == 1) // increase
-			{
-				if (gear <= 0.97)
-					gear += 0.03;
-				else
-					gear = 1;
-				gear_pressed = true;
-			}
-		}
-		
-		lin_vel = lin_vel * gear;
-		turnFactor_x = turnFactor_x * gear;
-		ROS_INFO("Linear velocity: %f", (lin_vel/100));
-		twist.linear.x = static_cast<double>(lin_vel/(double)255.0)*MAX_LINEAR_SPEED; // Should be in range of -MAX_LINEAR_SPEED to +MAX_LINEAR_SPEED 
-		twist.angular.z = static_cast<double>(turnFactor_x)*MAX_ANGULAR_SPEED; // Should be in range of -MAX_ANGULAR_SPEED to +MAX_ANGULAR_SPEED 
-
-		// ROS_INFO("Turn Factor X %f", turnFactor_x);
-		// ROS_INFO("Turn Factor Y %f", turnFactor_y);
-		// ROS_INFO("Motor Value %f", lin_vel);
-		ROS_INFO("GEAR %f", gear);
-
-		drive_pub.publish(twist);
-	}
+	buttons = joy->buttons;
+	axes = joy->axes;
+	// ROS_INFO("Joy Callback %f", axes[0]);
+	
 }
 
 void TeleopRover::networkCallback(const std_msgs::Bool::ConstPtr& net_stat){
@@ -149,9 +87,207 @@ void TeleopRover::stateCallback(const rover::StateMsg::ConstPtr& state_msg){
 	MANUAL_ENABLED = state_msg->MANUAL_ENABLED;
 }
 
+void TeleopRover::SetVelocity(){
+	ros::Rate loop_rate(20);
+	while (ros::ok())
+	{
+		int KILL = 10; // PS button to kill (0 velocity) as soon as it is pressed
+		if (buttons[KILL] == 1){
+			if (KILL_PRESSED == false) {
+				ROS_INFO("KILL ENGAGED");
+				twist.linear.x = 0;
+				twist.linear.y = 0;
+				twist.linear.z = 0;
+				twist.angular.x = 0;
+				twist.angular.y = 0;
+				twist.angular.z = 0;
+				drive_pub.publish(twist);	
+				KILL_PRESSED = true;
+			} else {
+				ROS_INFO("KILL DISENGAGED");
+				KILL_PRESSED = false;
+			}
+		}
+
+		if (MANUAL_ENABLED && ~KILL_PRESSED){
+			twist.linear.x = 0; 
+			twist.linear.y = 0;
+			twist.linear.z = 0;
+			twist.angular.x = 0;
+			twist.angular.y = 0;
+			twist.angular.z = 0;
+			// indexs for controller values
+			int R2 = 5;
+			int L2 = 2;
+			int LS_x = 0; // x axis of left joystick
+			int LS_y = 1; // y axis of left joystick
+			int dec_speed = 4; // L1
+			int inc_speed = 5; // R1
+
+			// Values from Controller
+			double posThrottle = static_cast<double>(axes[R2]);
+			double negThrottle = static_cast<double>(axes[L2]);
+			double turnFactor_x = static_cast<double>(axes[LS_x]);
+			double turnFactor_y = static_cast<double>(axes[LS_y]);
+			ROS_INFO("LS_x:%f", turnFactor_x);
+			ROS_INFO("LS_y:%f", turnFactor_y);
+
+			posThrottle = (posThrottle + 1) / 2; // Normalizing values from 0 to 1
+			negThrottle = (negThrottle + 1) / 2; // Normalizing values from 0 to 1
+			posThrottle = 1 - posThrottle; // Inverting values because posThrottle is 1 when not pressed, 0 when completely pressed
+			negThrottle = 1 - negThrottle; // Inverting values
+			ROS_INFO("R2:%f", posThrottle);
+			ROS_INFO("L2:%f", negThrottle);
+
+			double max_allowed_speed = gear * MAX_LINEAR_SPEED; // Maximum speed allowed for the gear
+			double acc = 0; // Acceleration value based on how much R2 is pressed
+
+			if (gear_pressed)
+			{
+				if (buttons[dec_speed] != 1 && buttons[inc_speed] != 1)
+				{
+					gear_pressed = false;
+				}
+			}
+			else
+			{
+				// Encoding values for gear selection (range 0 - 1)
+				if (buttons[dec_speed] == 1) // decrease
+				{
+					if (gear >= 0.1) // Change this value to change the gear step
+						gear -= 0.1;
+					else
+						gear = 0;
+					gear_pressed = true;
+				}
+				else if (buttons[inc_speed] == 1) // increase
+				{
+					if (gear <= 0.9)
+						gear += 0.1;
+					else
+						gear = 1;
+					gear_pressed = true;
+				}
+			}
+
+			if (posThrottle != 0 && negThrottle != 0)
+			{
+				lin_vel = prev_lin_vel;
+				ROS_INFO("Both Throttle Pressed %f", lin_vel);
+			}
+			else if (posThrottle !=0)
+			{
+				acc = posThrottle * MAX_ACCELERATION;
+				if ((lin_vel + acc) < max_allowed_speed)
+				{
+					// ROS_INFO("acc = %f", acc);
+					// ROS_INFO("lin_vel = %f", lin_vel);
+					lin_vel = prev_lin_vel + acc;
+					prev_lin_vel = lin_vel;
+				}
+				else
+				{
+					// This is for the case when the gear is decreased while the throttle is pressed
+					// the max allowed speed decreases, so the speed should decelerate instead of instantly decrease
+					if (prev_lin_vel <= max_allowed_speed)
+					{
+						lin_vel = max_allowed_speed;
+						prev_lin_vel = lin_vel;
+					}
+					else
+					{
+						// ROS_INFO("Gear reduced\n\n\n");
+						lin_vel = prev_lin_vel - MAX_ACCELERATION;
+						// ROS_INFO("lin_vel = %f", lin_vel);
+						prev_lin_vel = lin_vel;
+					}
+				}
+				ROS_INFO("Positive Throttle Pressed %f", lin_vel);
+			}
+			else if (negThrottle != 0)
+			{
+				acc = negThrottle * MAX_ACCELERATION;
+				// ROS_INFO("lin_vel = %f", std::fabs(lin_vel - acc));
+				if ((std::fabs(lin_vel - acc)) < max_allowed_speed) // floating point aboslute value - fabs
+				{
+					lin_vel = prev_lin_vel - acc;
+					prev_lin_vel = lin_vel;
+				}
+				else
+				{
+					if (std::fabs(prev_lin_vel) <= max_allowed_speed)
+					{
+						lin_vel = -max_allowed_speed;
+						prev_lin_vel = lin_vel;
+					}
+					else
+					{
+						// This is to prevent from going to infinity when the gear is decreased to 0 and the prev_lin_vel becomes greater than 0 (when we do +MAX_ACCELERATION)
+						if (lin_vel < 0)
+						{
+							lin_vel = prev_lin_vel + MAX_ACCELERATION;
+							prev_lin_vel = lin_vel;
+						}
+						else
+						{
+							lin_vel = 0;
+							prev_lin_vel = lin_vel;
+						}
+					}
+				}
+				ROS_INFO("Negative Throttle Pressed %f", lin_vel);
+			}
+			else
+			{
+				if (lin_vel != 0)
+				{
+					if (lin_vel > 0)
+					{
+						lin_vel = prev_lin_vel - MAX_ACCELERATION;
+						prev_lin_vel = lin_vel;
+						if (lin_vel < 0)
+						{
+							lin_vel = 0;
+							prev_lin_vel = 0;
+						}
+					}
+					else
+					{
+						lin_vel = lin_vel + MAX_ACCELERATION;
+						prev_lin_vel = lin_vel;
+						if (lin_vel > 0)
+						{
+							lin_vel = 0;
+							prev_lin_vel = 0;
+						}
+					}
+				}
+				
+				ROS_INFO("No Throttle Pressed %f", lin_vel);
+			}
+			
+			turnFactor_x = turnFactor_x * gear;
+			ROS_INFO("Linear velocity: %f", (lin_vel));
+			twist.linear.x = static_cast<double>(lin_vel); // Should be in range of -MAX_LINEAR_SPEED to +MAX_LINEAR_SPEED 
+			twist.angular.z = static_cast<double>(turnFactor_x)*MAX_ANGULAR_SPEED; // Should be in range of -MAX_ANGULAR_SPEED to +MAX_ANGULAR_SPEED 
+
+			// ROS_INFO("Turn Factor X %f", turnFactor_x);
+			// ROS_INFO("Turn Factor Y %f", turnFactor_y);
+			// ROS_INFO("Motor Value %f", lin_vel);
+			ROS_INFO("GEAR %f", gear);
+
+			drive_pub.publish(twist);
+		}
+		ros::spinOnce(); // for some reason, without this line, the code just remains in this loop and doesn't exit which doesn't let the subscriber to get data
+		loop_rate.sleep();
+	}
+
+}
+
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "drive_sender_falcon");
+	ros::init(argc, argv, "drive_sender_node");
 	TeleopRover drive_sender;
-	ros::spin();
+	drive_sender.SetVelocity();
+	// ros::spin(); // no need for this since we have a while loop in SetVelocity function
 }
