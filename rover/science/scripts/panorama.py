1835a8af96a4caf85f40a5aa1f60d49162b71c12
Liu-Fayi <liufy520@hotmail.com>
2024-09-21 14:05:00 -0400

    automated panorama initial commit
    ready for testing

---
+++ rover/science/scripts/panorama.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import stitcher
+import rospy
+from sensor_msgs.msg import Image
+from sensor_msgs.msg import Twist
+import time
+
+# ros subscriber
+class Subscriber:
+    def __init__(self):
+        self.sub = rospy.Subscriber('rgb/image_rect_color', Image, self.callback)
+        self.imgfiles = []
+        self.save = False
+    
+    def callback(self, data):
+        print('Received image')
+        time.sleep(0.5)
+        if self.save:
+            print('Image ' + len(self.imgfiles))
+            self.imgfiles.append(data)
+
+    def save(self):
+        self.save = True
+
+    def stop(self):
+        self.save = False
+
+    def reset(self):
+        self.imgfiles = []
+        
+# ros publisher
+class Publisher:
+    def __init__(self):
+        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
+    def turn(self, angular_vel):
+        twist = Twist()
+        twist.linear.x = 0
+        twist.angular.z = angular_vel
+        self.pub.publish(twist)
+
+    def stop(self):
+        twist = Twist()
+        twist.linear.x = 0
+        twist.angular.z = 0
+        self.pub.publish(twist)
+
+
+class Panorama:
+    def __init__(self, sub: Subscriber, pub: Publisher):
+        self.sub:Subscriber = sub
+        self.pub:Publisher = pub
+        self.stitcher = stitcher.Stitcher()
+
+    def start(self, num_images):
+        print('Starting panorama')
+        self.sub.reset()
+        self.sub.save()
+        for i in range(num_images):
+            self.sub.stop()
+            self.pub.turn(0.1)
+            time.sleep(5)
+            self.pub.stop()
+            self.sub.save()
+            time.sleep(0.5)
+        self.sub.stop()
+        self.stitcher.stitch(self.sub.imgfiles)
+        print('Panorama completed')
+
+def main():
+    rospy.init_node('panorama')
+    sub = Subscriber()
+    pub = Publisher()
+    pan = Panorama(sub, pub)
+    pan.start(5)
+
+if __name__ == '__main__':
+    main()
---
+++ rover/science/scripts/stitcher.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+# from __future__ import print_function
+
+import cv2 as cv
+
+# modes = (cv.Stitcher_PANORAMA, cv.Stitcher_SCANS)
+ 
+# parser = argparse.ArgumentParser(prog='stitcher.py', description='Stitching sample.')
+# parser.add_argument('--mode',
+#     type = int, choices = modes, default = cv.Stitcher_PANORAMA,
+#     help = 'Determines configuration of stitcher. The default is `PANORAMA` (%d), '
+#          'mode suitable for creating photo panoramas. Option `SCANS` (%d) is suitable '
+#          'for stitching materials under affine transformation, such as scans.' % modes)
+# parser.add_argument('--output', default = 'result.jpg',
+#     help = 'Resulting image. The default is `result.jpg`.')
+# parser.add_argument('img', nargs='+', help = 'input images')
+ 
+# __doc__ += '\n' + parser.format_help()
+class Stitcher:
+    def __init__(self, panaroma = True):
+        if panaroma:
+            self.mode = cv.Stitcher_PANORAMA
+        else:
+            self.mode = cv.Stitcher_SCANS
+
+    def stitch(self, imgfiles):
+        # args = parser.parse_args()
+    
+        # read input images
+        imgs = []
+        # imgfiles = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg']
+        for img_name in imgfiles:
+            # img = cv.imread(cv.samples.findFile(img_name))
+            img = cv.imread(img_name)
+            if img is None:
+                print("can't read image " + img_name)
+                return
+                # sys.exit(-1)
+            imgs.append(img)
+    
+        #![stitching]
+        # stitcher = cv.Stitcher.create(args.mode)
+        stitcher = cv.Stitcher_create(self.mode)
+        status, pano = stitcher.stitch(imgs)
+    
+        if status != cv.Stitcher_OK:
+            print("Can't stitch images, error code = %d" % status)
+            return
+            # sys.exit(-1)
+        #![stitching]
+    
+        cv.imwrite("result.jpg", pano)
+        print("stitching completed successfully, file saved!")
+    
+        print('Done')
+ 
+ 
+# if __name__ == '__main__':
+#     print(__doc__)
+#     stitch()
+#     cv.destroyAllWindows()
