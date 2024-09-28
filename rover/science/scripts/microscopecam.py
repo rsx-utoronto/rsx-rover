72149f949dfe3310339682af00e54a819f296dcb
Jason Li <jasonli.0616@outlook.com>
2024-09-28 16:56:13 -0400

    Microscope camera publisher

---
+++ rover/science/scripts/microscopecam.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+
+import rospy
+from sensor_msgs.msg import Image
+import cv2
+from cv_bridge import CvBridge
+
+class MicroscopeCam:
+
+    def __init__(self):
+        self.rate = rospy.Rate(10)
+        self.pub = rospy.Publisher("microscope", Image, queue_size=10)
+
+
+        # Get camera feed
+        # TODO: Get camera based on "AVEO_Technology_Corp._USB2.0_Camera" rather than hard-coded
+        camera = cv2.VideoCapture(0)
+
+        # Create bridge object
+        bridge = CvBridge()
+
+        while not rospy.is_shutdown():
+
+            ret, frame = camera.read()
+
+            if ret: # if frame is read correctly
+
+                # Convert with ros bridge and publish
+                self.pub.publish(bridge.cv2_to_imgmsg(frame, "bgr8"))
+
+                self.rate.sleep()
+
+
+if __name__ == "__main__":
+    try:
+        rospy.init_node("microscopecam")
+        microscopecam = MicroscopeCam()
+    except rospy.ROSInterruptException:
+        pass
