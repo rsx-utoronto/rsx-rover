d238eedbb6f1f0ede3061d00e16590743b9817a8
erisdefne <defne.eriss@gmail.com>
2024-12-28 21:27:41 -0500

    treat grid search like get targets

--- rover/autonomy/scripts/sm_grid_search.py
+++ rover/autonomy/scripts/sm_grid_search.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python3
+
 import rospy
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Float64MultiArray
 import math
-from straight_line_approach import StraightLineApproach
 from nav_msgs.msg import Odometry
 
 class GridSearch:
@@ -17,22 +18,11 @@ class GridSearch:
         self.x = w 
         self.y = h
         self.tol = tolerance 
-        self.straight_line_approach = StraightLineApproach(lin_vel, ang_vel, self.square_target)
-        self.odom_subscriber = rospy.Subscriber('/rtabmap/odom', Odometry, self.odom_callback)
-
-    def odom_callback(self, msg):
-        self.x = msg.pose.pose.position.x
-        self.y = msg.pose.pose.position.y
-        self.heading = self.to_euler_angles(msg.pose.pose.orientation.w, msg.pose.pose.orientation.x,
-                                            msg.pose.pose.orientation.y, msg.pose.pose.orientation.z)[2]
 
     '''
     Generates a list of targets for straight line approach.
     '''
     def square_target(self):
-        # notice, up/down is odd amount, left/right is even amount        
-        
-        factor = self.tol
         x, y, dx, dy = 0, 0, 0, 1
         targets = [(x,y)]
         step = 1
@@ -48,6 +38,7 @@ class GridSearch:
             step += 1  
         return targets
 
+        # notice, up/down is odd amount, left/right is even amount        
         # while len(targets) < self.x: # 1, 2, 3, 4, 5... sol 
         #     if step%2==1: # moving up/down
         #         for i in range (step):
@@ -65,6 +56,3 @@ class GridSearch:
         #                     return targets
         #     step += 1  # Increase step size after two edges
         # return targets
-
-    def search(self):
-        self.straight_line_approach.navigate()
