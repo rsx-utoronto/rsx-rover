687a656ac5d45e05b1c8cce661a7dd300e02131f
Garvishb <togarvish@gmail.com>
2024-12-29 16:15:03 -0500

    Revert "Merge branch 'integrating_micro_camera' into feature/obstacle_avoidance"
    This reverts commit e9bd2d1fa057ee96431735727051f91ed0080d73, reversing
    changes made to a9ffe49c830dc7e741105e52c018bb394404e407.

--- scripts/Yolov8_test.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-
-model = YOLO('yolov8s.pt')  # Load model
-
-
-results = model.train(data=r'/home/rsx-base/rsx-rover/scripts/nov16.yaml', epochs=300, batch=16, device=0)
-#originally: was epochs 200 , bacth 16
