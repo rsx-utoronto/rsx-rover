14b5a6a3a40dbb5d769baca068e304bf27796e17
Parm345 <parmghai@protonmail.com>
2025-02-14 07:31:48 -0500

    updated IK to work with 2025 arm

--- rover/arm/ros1/Visualization/arm_viz.py
+++ rover/arm/ros1/Visualization/arm_viz.py
@@ -156,7 +156,7 @@ def runNewDesiredJointState(jointPublisherData, angles):
     newJointState = JointState()
     newJointState.header = Header()
     newJointState.header.stamp = rospy.Time.now()
-    newJointState.name = ["Joint_1", "Joint_2", "Joint_3", "Joint_4", "Joint_5", "Joint_6"]
+    newJointState.name = ["joint_1", "joint_2", "joint_3", "joint_4", "joint_5", "joint_6"]
     # Angles in radians [Joint_1, Joint_2, ....], re-run this script and change the values to see it work.
     newJointState.position = angles
     jointPublisherData.publish(newJointState)  # send data to be published
