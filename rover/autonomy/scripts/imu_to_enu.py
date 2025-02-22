37454f5c5e06ecea65241fe246da85181d4b80e2
Fayi Liu <liufy520@hotmail.com>
2025-02-22 14:59:10 -0500

    getting cov

--- rover/autonomy/scripts/imu_to_enu.py
+++ rover/autonomy/scripts/imu_to_enu.py
@@ -2,6 +2,7 @@
 
 import rospy
 from sensor_msgs.msg import Imu
+import tf
 
 
 class ImuDataToENU:
@@ -21,7 +22,33 @@ class ImuDataToENU:
         transformed.angular_velocity.x = -data.angular_velocity.y
         transformed.angular_velocity.y = data.angular_velocity.x
         transformed.angular_velocity.z = data.angular_velocity.z
+        # new_orientation = self.transform_orientation(data.orientation)
+        # transformed.orientation.x = new_orientation[0]
+        # transformed.orientation.y = new_orientation[1]
+        # transformed.orientation.z = new_orientation[2]
+        # transformed.orientation.w = new_orientation[3]
+        transformed.linear_acceleration_covariance = self.transform_covariance(data.linear_acceleration_covariance)
+        transformed.angular_velocity_covariance = self.transform_covariance(data.angular_velocity_covariance)
+        # transformed.orientation_covariance = self.transform_covariance(data.orientation_covariance)
         self.pub.publish(transformed)
+
+    # def transform_orientation(self, orientation):
+    #     rpy = tf.transformations.euler_from_quaternion([orientation.x, orientation.y, orientation.z, orientation.w])
+    #     new_orientation = tf.transformations.quaternion_from_euler(-rpy[1], rpy[0], rpy[2])
+    #     return new_orientation
+
+    def transform_covariance(self, covariance):
+        new_covariance = [0] * 9
+        new_covariance[0] = covariance[4]
+        new_covariance[1] = covariance[3]
+        new_covariance[2] = covariance[5]
+        new_covariance[3] = covariance[1]
+        new_covariance[4] = covariance[0]
+        new_covariance[5] = covariance[2]
+        new_covariance[6] = covariance[7]
+        new_covariance[7] = covariance[6]
+        new_covariance[8] = covariance[8]
+        return new_covariance
 
 if __name__ == '__main__':
     rospy.init_node('imu_to_enu')
