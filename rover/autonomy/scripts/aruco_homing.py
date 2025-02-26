8498eae54bc8fe8d5fb8bf7ed23a2519456ebb21
garvishb <togarvish@gmail.com>
2025-02-25 21:59:14 -0500

    homing works for bottle, recorded compressed images

--- rover/autonomy/scripts/aruco_homing.py
+++ rover/autonomy/scripts/aruco_homing.py
@@ -127,7 +127,7 @@ def main():
     # frame_width, frame_height, min_aruco_area, aruco_min_x_uncert, aruco_min_area_uncert, max_linear_v, max_angular_v
     # aimer = AimerROS(640, 360, 1000, 100, 100, 1.8, 0.8) # FOR ARUCO
     
-    aimer = AimerROS(50, 50, 1450, 10, 50, 1.0, 0.5) # FOR WATER BOTTLE
+    aimer = AimerROS(640, 360, 1450, 50, 200, 1.0, 0.5) # FOR WATER BOTTLE
     rospy.Subscriber('aruco_node/bbox', Float64MultiArray, callback=aimer.rosUpdate) # change topic name
     # int32multiarray convention: [top_left_x, top_left_y, top_right_x, top_right_y, bottom_left_x, bottom_left_y, bottom_right_x, bottom_right_y]
     rate = rospy.Rate(10)
