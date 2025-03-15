7da001ee6dcbd1bf02b91c5510e990ccc0203efc
Defne <defne.eriss@gmail.com>
2025-03-15 14:49:04 -0400

    Correct windows code

--- rover/autonomy/scripts/led_light.py
+++ rover/autonomy/scripts/led_light.py
@@ -7,39 +7,56 @@
 import time
 import serial
 import subprocess 
-from std_msgs.msg import String
-import rospy
+import serial.tools.list_ports
+#from std_msgs.msg import String
+#import rospy
 
 #Make a subscriber in here instead of the State Machine file
+
 #For Windows
-def windows_get_led_port(led_name):
-    #For linux
-    # Run the v4l2-ctl command and capture the output
-    command = "Get-WMIObject Win32_PnPEntity | Where-Object { $_.DeviceID -match '^USB' } | Select-Object DeviceID, Name"
 
-    result = subprocess.run(["powershell", "-Command", command], capture_output=True, text=True)
+def list_all_ports():
+	ports = [port.device for port in serial.tools.list_ports.comports()]
+	return ports
 
-    # Split the output into lines
-    lines = result.stdout.splitlines()
+def windows_get_led_port():
+  item_name = "USB\\VID_0483&PID_374B&MI_02\\6&348E67AE&0&0002"
+
+  command = "Get-WMIObject Win32_SerialPort"
+
+  result = subprocess.run(["powershell", "-Command", command], capture_output=True, text=True)
+
+  connected_items = result.stdout.splitlines()
+  i = 0
+  port_name = ""
+  while (i < len(connected_items)):
+    if connected_items[i] == "":
+      pass
+    if item_name in connected_items[i]:
+       port_id_list = connected_items[i-11].split(": ")
+       port_name = port_id_list[len(port_id_list)-1]
+       break
+    i+=1
+
+  ports = list_all_ports()
+  if len(ports) == 0:
+    print("No port found. Exiting.")
+    import sys
+    sys.exit(0)
+  elif len(ports) > 1:
+    print("There are multiple ports connected")
+    for port in ports:
+       if port == port_name:
+          print(port)
+          return port
     
-    led_device = None
-    found_led = False
+  else:
+    print("One port found")
+    port = ports[0]
+    if port == port_name:
+          print(port)
+          return port
 
-    for line in lines:
-        # Check for the led device header
-        if led_name in line:
-            found_led = True
-            
-        
-        # If we are in the led section, look for the device path
-        if found_led:
-            led_device = line.split(' ') [0]
-            break  # Stop after getting the first device path
-
-    print(led_device)
-    return led_device
-
-#For Linux
 
 def linux_get_led_port(led_name):
     # Run the v4l2-ctl command and capture the output
@@ -65,44 +82,44 @@ def linux_get_led_port(led_name):
 
     return led_device
 
-def state_callback(msg):
-  #serial_port = linux_get_led_port(led_name)
-  #serial_port = windows_get_led_port(led_name)
-  serial_port = "/dev/ttyUSB0" #Find out the seriel_port
-  baud_rate = 9600
-  ser = serial.Serial(serial_port, baud_rate, timeout=1)
+def state_callback(mode):
+  #seried_port = linux_get_led_port(led_name)
+  serial_port = windows_get_led_port()
+  #serial_port = "/dev/ttyUSB0" #Find out the seriel_port
+  board = serial.Serial(port=serial_port, baudrate=115200, timeout=1)
 
-  mode = msg.data
-  print(mode)
+  # mode = msg.data
+#   print(mode)
   #global res
   #mode = msg.rover_mode stm32
   if mode == 'mission done':
     res = 'green\n' #How to get it to flash??
     for i in range(3):
-      print(res)
-      ser.open()
-      ser.write(res.encode()) #Figure out the communication method
-      ser.close()
+      if i > 0:
+         board.open()
+      board.write(bytes(res, 'utf-8')) 
+      board.close()
       time.sleep(0.3) 
-      ser.open()   
-      ser.write(bytes([0])) #should reset the LED
-      ser.flush() # Ensure it's sent before closing
-      ser.close()
+      board.open()   
+      board.write(bytes('off\n', 'utf-8'))
+      board.flush() # Ensure it's sent before closing
+      board.close()
       time.sleep(0.3)
     
   elif mode == 'auto': 
     res = 'red\n'
     print(res)
-    ser.open()
-    ser.write(res.encode()) #Figure out the communication method
-    ser.close() 
-  #else:
-  #  res = 'blue\n' 
+    board.write(bytes(res, 'utf-8'))
+  else:
+    res = 'blue\n' 
+    print(res)
+    board.write(bytes(res, 'utf-8'))
 
 def main():
-   rospy.init_node('led_listener', anonymous=True)
-   led_subscriber = rospy.Subscriber("led_light", String, state_callback)
-   rospy.spin()
+   #rospy.init_node('led_listener', anonymous=True)
+   #led_subscriber = rospy.Subscriber("led_light", String, state_callback)
+   state_callback('auto')
+   #rospy.spin()
 
 
 #Is this part needed?
@@ -112,6 +129,4 @@ def main():
 #rospy.spin()
 
 if __name__ == "__main__":
-   #linux_get_led_port("COM3")
    main()
-
