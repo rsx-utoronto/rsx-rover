321558e916ba30d9d934781da63578e4166428cf
garvishb <togarvish@gmail.com>
2025-03-29 15:21:43 -0400

    ggs

--- scripts/label_sorting.py
+++ scripts/label_sorting.py
@@ -1,11 +1,13 @@
+#!/usr/bin/python3
+
 import os
 import shutil
 
 # =======================
 # Configurable Variables
 # =======================
-BASE_DIR = "updated_data"       # Folder containing test, train, val
-LABELS_DIR = "Labels"           # Folder containing label files
+BASE_DIR = "/rover_ws/src/comp_data/rsx-rover/updated_data"       # Folder containing test, train, val
+LABELS_DIR = "/rover_ws/src/comp_data/rsx-rover/Labels"           # Folder containing label files
 IMAGE_FOLDER_NAME = "images"    # Folder name inside each subset that contains images
 OUTPUT_LABELS_SUBDIR = "labels" # Subfolder where label files will be copied
 LABEL_EXTENSION = ".txt"        # Extension for label files (adjust if needed)
