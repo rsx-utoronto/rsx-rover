0a080ceab68f73a3bbb0910076e05f4ec364891f
Parm345 <parmghai@protonmail.com>
2025-04-19 02:31:43 -0400

    got arm ik mathworking, time to sim stuff before using

---
+++ rover/arm/ros1/IK/arm_sci.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+
+import rospy
+
+from arm_science_ik import SciArm
+from queue import Queue
+from std_msgs.msg import Float32MultiArray, String
+from numpy import deg2rad, rad2deg
+from rover.msg import ArmInputs
+
+class ArmSciNode():
+    def __init__(self):
+        self.armState = "Idle"
+        self.joyInputQueue = Queue()
+        self.curAngleQueue = Queue()
+        dhTable = [[79.7, 0, 0, 0],
+                   [0,    0, 367, 0],
+                   [0,    0, 195, 0],
+                   [0,    0, 67, 0],
+                   [92,   0, 0, 0]]
+
+        self.BUTTON_NAMES = ["X", "CIRCLE", "TRIANGLE", "SQUARE", "L1", "R1", "L2", "R2", "SHARE", "OPTIONS", "PLAY_STATION", "L3", 
+                             "R3", "UP", "DOWN", "LEFT", "RIGHT"]
+        self.buttonStatus= {"X": False, "CIRCLE": False, "TRIANGLE": False, "SQUARE": False, "L1": False, "R1": False, "L2": False, 
+                               "R2": False, "SHARE": False, "OPTIONS": False, "PLAY_STATION": False, "L3": False, "R3": False,"UP": False, 
+                               "DOWN": False, "LEFT": False, "RIGHT": False} 
+
+        self.arm = SciArm(5, dhTable)
+
+        rospy.init_node("arm_sci")
+        self.rate = rospy.Rate(30)
+
+        self.goalPub = rospy.Publisher("arm_goal_pos", Float32MultiArray, queue_size=10)
+
+        rospy.Subscriber("arm_state", String, self.onArmStateUpdate)
+        rospy.Subscriber("arm_inputs", ArmInputs, self.onJoystickUpdate)
+        rospy.Subscriber("arm_curr_pos", Float32MultiArray, self.onCurrPosUpdate)
+
+    def getJoystickButtonStatus(self, tempButton:list) -> dict: # setting up the buttons
+        ''' Gets the Status of the Pressed Buttons on Joystick
+
+        If the value in the returned dictionary for a button is 
+            2: button was just pressed
+            1: button is pressed
+            0: button is not pressed
+            -1: button was just released 
+
+        Returns
+        -------
+        dictionary
+            dictionary with keys for buttons from BUTTON_NAMES, value is either 2, 1, 0, or -1
+        
+        '''
+        buttons = {"X": 0, "CIRCLE": 0, "TRIANGLE": 0, "SQUARE": 0 , "L1": 0, "R1": 0, "L2": 0, "R2": 0, "SHARE": 0, "OPTIONS": 0, "PLAY_STATION": 0, 
+            "L3": 0, "R3": 0,"UP": 0, "DOWN": 0, "LEFT": 0, "RIGHT": 0 } # 1 is pressed, 0 is not pressed, -1 is just released
+    
+        for i in range(0, tempButton.__len__()):
+            button = tempButton[i]
+            if self.buttonStatus[self.BUTTON_NAMES[i]] == True and button == 0: # button just released
+                button = -1
+            if self.buttonStatus[self.BUTTON_NAMES[i]] == False and button == 1: # button just pressed
+                button = 2
+
+            if button < 1:
+                self.buttonStatus[self.BUTTON_NAMES[i]] = False
+            else:
+                self.buttonStatus[self.BUTTON_NAMES[i]] = True
+        
+            buttons[self.BUTTON_NAMES[i]] = button
+            
+        return buttons
+
+    # ROS Topic Subscriptions
+    def onCurrPosUpdate(self, data:Float32MultiArray):
+        # don't forgoet to
+        self.curAngleQueue.put(deg2rad(data.data))
+
+    def onArmStateUpdate(self, data):
+        self.armState = data.data
+        if self.armState == "Idle" or self.armState == "Manual":
+            self.arm.setMode("Forwad") 
+            with self.joyInputQueue.mutex:
+                self.joyInputQueue.queue.clear()
+        if self.armState == "IK":
+            self.arm.setMode("IK")
+
+    def onJoystickUpdate(self, data):
+        ''' Callback function for the arm_inputs topic'''
+
+        global movementSpeed
+        global isMovementNormalized
+        global curArmAngles
+        global newTargetValues
+
+        buttonsPressed = [0, 0, data.triangle, 0, data.l1, data.r1, data.l2, data.r2, data.share, data.options, 0, 0, 0, 0, 0, 0, 0]
+        # isButtonPressed = {"X": data.x, "CIRCLE": data.o, "TRIANGLE": data.triagle, 
+        #                         "SQUARE": 0, "L1": data.l1, "R1": data.r1, "L2": data.l2, 
+        #                         "R2": data.r2, "SHARE": data.share, "OPTIONS": data.options, 
+        #                         "PLAY_STATION": 0, "L3": 0, "R3": 0, "UP": 0, "DOWN": 0, 
+        #                         "LEFT": 0, "RIGHT": 0} 
+        isButtonPressed = self.getJoystickButtonStatus(buttonsPressed)
+        # print(isButtonPressed)
+        joystickAxesStatus = {"L-Right": -data.l_horizontal, "L-Down": -data.l_vertical, "L2": data.l2, 
+                            "R-Right": -data.r_horizontal, "R-Down": -data.r_vertical, "R2": data.r2}
+        
+        self.joyInputQueue.put([isButtonPressed, joystickAxesStatus])
+
+    # Main Loop Code
+
+    def updateTarget(self, joystickData):
+        pass
+
+    def main(self):
+        while not rospy.is_shutdown():
+            # deal with controller input queue 
+            # do IK
+            # publish target if state allows it
+
+            if not self.curAngleQueue.empty():
+                self.arm.setCurAngles(self.curAngleQueue.get())
+
+            
+            if not self.joyInputQueue.empty():
+                [buttonPressed, joystickStatus] = self.joyInputQueue.get()
+
+                if buttonPressed["TRIANGLE"]:
+                    self.arm.iterateMode()
+                    print(self.arm.curMode)
+
+                if self.armState == "IK" and self.arm.getCurMode() == "Cyl":
+                    self.arm.controlTarget(buttonPressed, joystickStatus)
+                    status = self.arm.inverseKinematics()
+                    goalAngles = rad2deg(self.arm.getGoalAngles())
+                    rospy.loginfo(status)
+                    # rospy.loginfo(self.arm.cylTarget)
+
+                    goalAngles.append(0) # end effector
+
+                    goalTopicData = Float32MultiArray()
+                    goalTopicData.data = goalAngles
+                    self.goalPub.publish(goalTopicData)
+            
+            if self.armState != "IK":
+                self.arm.forwardKinematics()
+
+            # rospy.loginfo(self.arm.cylTarget)
+            self.rate.sleep()
+
+        
+
+if __name__ == "__main__":
+    node = ArmSciNode()
+    node.main()
