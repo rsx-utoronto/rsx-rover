fixed joint4 IK (flipped sign)

rover/arm/ros1/IK/ik_test.py
@@ -10,11 +10,11 @@ dhTable = [[79.7, 0, 0, pi/2],
 offsets = [0,
            -atan2(112.99, 348.08), 
            atan2(161, 110.7) + atan2(112.99, 348.08),
-           -(atan2(92, 67) - atan2(110.75, 161)),
+           (atan2(92, 67) + atan2(110.75, 161)),
            0]
 
 arm = SciArm(5, dhTable, offsets)
-arm.setTarget([0 , 629, -12.3, 0.941354])
+arm.setTarget([0 , 550, -12.3, 0.941354])
 status = arm.inverseKinematics()
 print(status)
 goalAngles = arm.getGoalAngles()
@@ -25,7 +25,7 @@ print(goalAngles, type(goalAngles), "\n")
 # arm.forwardKinematics()
 # forwardKin = np.round(arm.calculateTransformToLink(3), 2)
 # print(forwardKin, "\n")
-goalAngles[3] = goalAngles[3] - 0.941354
+goalAngles[3] = goalAngles[3] + 0.941354
 arm.setCurAngles(goalAngles)
 arm.forwardKinematics()
 forwardKin = np.round(arm.calculateTransformToLink(5), 2)