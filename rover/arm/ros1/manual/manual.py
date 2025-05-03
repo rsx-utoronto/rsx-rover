5adca8fea37063fc9c3346ac968b32b5f3a1fc07
abhay126 <vabhay.12601@gmail.com>
2025-05-03 14:55:18 -0400

    update speeds

--- rover/arm/ros1/manual/manual.py
+++ rover/arm/ros1/manual/manual.py
@@ -52,7 +52,7 @@ class Manual():
 
         ## Constant Speed limits, values are chosen by trial and error #TODO Find value for 7th motor
 
-        self.SPEED_LIMIT         = [-0.2, 0.2, 0.5, 0.75, 0.12, 0.8, 400]
+        self.SPEED_LIMIT         = [0.2, 0.2, 0.5, 0.75, 0.12, 0.8, 400]
 
         ## Variable for the status, start at idle
         self.status              = "Idle"
