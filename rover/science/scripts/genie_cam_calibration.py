a57933c6b40cb0e6ee939ea4ca50eb9e76b748c5
Liu-Fayi <85021501+Liu-Fayi@users.noreply.github.com>
2025-05-28 09:25:07 -0400

    genie cam calibration

--- rover/science/scripts/genie_cam_calibration.py
+++ rover/science/scripts/genie_cam_calibration.py
@@ -18,6 +18,23 @@ How to use:
 4. Run the script to perform calibration and plot the spectral reflectance curve.
 '''
 
+relative_spectral_sensitivity = {
+    440: 0.78,
+    500: 0.97,
+    530: 1.0,
+    570: 0.96,
+    610: 0.9,
+    670: 0.76,
+    740: 0.58,
+    780: 0.44,
+    840: 0.31,
+    900: 0.18,
+    950: 0.1,
+    1000: 0.05
+}
+
+wavelengths = [440, 500, 530, 570, 610, 670, 740, 780, 840, 900, 950, 1000]
+
 def load_images(folder):
     # load all images in a folder as greyscale numpy arrays
     images = []
@@ -31,6 +48,27 @@ def mean_image(images):
     # compute the mean image from a stack of images
     return np.mean(images, axis=0)
 
+def get_reflectance(filepath: str) -> dict:
+    with open(filepath, "r") as f:
+        lines = f.read().split("\n")
+        d = {
+            440: 0.0,
+            500: 0.0,
+            530: 0.0,
+            570: 0.0,
+            610: 0.0,
+            670: 0.0,
+            740: 0.0,
+            780: 0.0,
+            840: 0.0,
+            900: 0.0,
+            950: 0.0,
+            1000: 0.0
+        }
+        for i in wavelengths:
+            d[i] = float(lines[i - 349])
+        return d
+
 def calibrate_images(
     bias_folder, dark_folder, flat_folder, calibration_folder, sample_folder, wavelengths, roi=None
 ):
@@ -43,23 +81,93 @@ def calibrate_images(
 
     # computing the mean frames
     bias = mean_image(bias_imgs)
-    dark = mean_image(dark_imgs) - bias
-    flat = mean_image(flat_imgs) - dark
-    flat_norm = flat / np.mean(flat)
+    dark_corr = mean_image(dark_imgs) - bias
+    flat_corr = mean_image(flat_imgs) - dark_corr
+    flat_norm = flat_corr / np.mean(flat_corr)
+
+    cv2.imwrite("../genie_calibration_data/dark_corr", dark_corr)
+    cv2.imwrite("../genie_calibration_data/flat_norm", flat_norm)
 
     # correct calibration and sample images
-    cal_corrected = (cal_imgs - dark) / flat_norm
-    sample_corrected = (sample_imgs - dark) / flat_norm
+    cal_corrected = (cal_imgs - dark_corr) / flat_norm
+    sample_corrected = (sample_imgs - dark_corr) / flat_norm
 
     # computing normalized reflectance (using reflectance transformation formula)
-    reflectance = sample_corrected / cal_corrected  # shape: (num_filters, H, W)
+    # reflectance = sample_corrected / cal_corrected  # shape: (num_filters, H, W)
+
+    for i in range(len(wavelengths)):
+        cal_corrected[i] /= relative_spectral_sensitivity[wavelengths[i]]
+        sample_corrected[i] /= relative_spectral_sensitivity[wavelengths[i]]
+    
+    with open("../genie_calibration_data/chalk - pigments checker acrylic - GorgiasUV.txt", "r") as fca:
+        chalk_reflectance = {
+            440: 0.0,
+            500: 0.0,
+            530: 0.0,
+            570: 0.0,
+            610: 0.0,
+            670: 0.0,
+            740: 0.0,
+            780: 0.0,
+            840: 0.0,
+            900: 0.0,
+            950: 0.0,
+            1000: 0.0
+        }
+        lines = fca.read().split("\n")
+        i = 0
+        min_diff = 1.0
+        val = 0
+        for line in lines[1:-2]:
+            l = line.split(" ")
+            diff = abs(float(l[0]) - wavelengths[i])
+            if diff < 1:
+                if diff < min_diff:
+                    min_diff = diff
+                    val = float(l[1]) / 100
+                else:
+                    chalk_reflectance[wavelengths[i]] = val
+                    i += 1
+    
+    hematite_reflectance = get_reflectance("../genie_calibration_data/s07_ASD_Hematite_GDS69.c_60-104um_BECKb_AREF.txt")
+    magnetite_reflectance = get_reflectance("../genie_calibration_data/s07_ASD_Magnetite_HS195.3B_BECKb_AREF.txt")
+    serpentine_reflectance = get_reflectance("../genie_calibration_data/s07_ASD_Magnetite_HS195.3B_BECKb_AREF.txt")
+    chlorite_reflectance = get_reflectance("../genie_calibration_data/s07_ASD_Magnetite_HS195.3B_BECKb_AREF.txt")
+
+    A = [0] * 12
+    B = [0] * 12
+    for i in range(12):
+        a = [
+            [chalk_reflectance[wavelengths[i]], 1],
+            [hematite_reflectance[wavelengths[i]], 1],
+            [magnetite_reflectance[wavelengths[i]], 1],
+            [serpentine_reflectance[wavelengths[i]], 1],
+            [chlorite_reflectance[wavelengths[i]], 1]
+        ]
+        A[i] = a
+    
+    B = np.transpose(B)
+
+    M = [0] * 12
+    C = [0] * 12
+    for i in range(12):
+        m, c = np.linalg.lstsq(A, B)
+        M[i] = m
+        C[i] = c
+
+    with open("../genie_calibration_data/calibration_constants.csv", "w") as f:
+        for i in range(12):
+            f.write(str(wavelengths[i]) + ", " + str(M[i]) + ", " + str(C[i]) + "\n")
+
+    reflectance = sample_corrected * M[0] + C[0]
+    reflectance_corr = np.divide(reflectance, flat_norm)
 
     # extract ROI (optional) and compute mean reflectance per wavelength
     if roi is not None:
         x1, y1, x2, y2 = roi  # ROI as (x1, y1, x2, y2)
-        reflectance_roi = reflectance[:, y1:y2, x1:x2]
+        reflectance_roi = reflectance_corr[:, y1:y2, x1:x2]
     else:
-        reflectance_roi = reflectance
+        reflectance_roi = reflectance_corr
 
     mean_reflectance = np.mean(reflectance_roi, axis=(1, 2)) # shape: (num_filters,)
     # plotting spectral reflectance curve
@@ -75,12 +183,11 @@ def calibrate_images(
 
 if __name__ == "__main__":
     # SET FOLDERS AND WAVELENGTHS HERE!!!   
-    bias_folder = "data/bias"
-    dark_folder = "data/dark"
-    flat_folder = "data/flat"
-    calibration_folder = "data/calibration_target"
-    sample_folder = "data/sample"
-    wavelengths = [450, 550, 650, 750, 850, 950, 1050, 1150, 1250, 1350, 1450, 1550]  # Example
+    bias_folder = "../data/bias"
+    dark_folder = "../data/dark"
+    flat_folder = "../data/flat"
+    calibration_folder = "../data/calibration_target"
+    sample_folder = "../data/sample"
 
     # optionally, set ROI as (x1, y1, x2, y2)
     roi = (100, 100, 400, 400)
