c9f08a30fd3f60c475ec6cea6049cecd5e7eb614
Fayi Liu <liufy520@hotmail.com>
2025-05-28 21:46:00 -0600

    geniecam bias and dark frames

--- rover/science/scripts/genie_cam_pic_saver.py
+++ rover/science/scripts/genie_cam_pic_saver.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
@@ -8,9 +10,11 @@ class PicSaver:
     def __init__(self):
         self.pic_sub=rospy.Subscriber('geniecam', Image, self.pic_sub_callback)
         self.counter = 0
-        self.bridge=CvBridge
-        self.num_photos = 12 #change to predetermined number of photos!
-        self.save_path = os.path.join(os.path.dirname(__file__), "calibrated_saved_images")
+        self.bridge=CvBridge()  # Create an instance of CvBridge
+        self.num_photos = 25 # TODO: change to predetermined number of photos!
+        self.save_path = os.path.join(os.path.dirname(__file__), "../genie_calibration_data")
+        self.save_path = os.path.abspath(self.save_path)  # Ensure absolute path
+        self.save_path = os.path.join(self.save_path, "dark") # change to desired save path
         os.makedirs(self.save_path, exist_ok=True)
 
     def pic_sub_callback(self, msg):
@@ -19,6 +23,7 @@ class PicSaver:
                 cv_image = self.bridge.imgmsg_to_cv2(msg)
                 filename = os.path.join(self.save_path, f"image_{self.counter:03d}.png")
                 cv2.imwrite(filename, cv_image)
+                print(f"Saved image {self.counter} to {filename}")
                 self.counter += 1
             except Exception as e:
                 rospy.logerr(f"Error saving image: {e}")
@@ -27,8 +32,3 @@ if __name__ == '__main__':
     rospy.init_node('pic_saver_node')
     ps = PicSaver()
     rospy.spin()
-        
-        
-        
-        
-        
