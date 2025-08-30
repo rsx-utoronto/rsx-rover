938c5b6ccb9bdaa924c1abd1039ee1d312ae82e3
garvishb <togarvish@gmail.com>
2025-08-30 15:47:26 -0400

    goose.yaml location changed

--- rover/drive/launch/falcons.py
+++ rover/drive/launch/falcons.py
@@ -3,6 +3,8 @@ from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     config_file = PathJoinSubstitution([
@@ -13,12 +15,18 @@ def generate_launch_description():
         'goose.yaml'
     ])
 
+    config = os.path.join(
+        get_package_share_directory('rover'),
+        'config',
+        'goose.yaml'
+    )
+
     return LaunchDescription([
         Node(
             package='ros_phoenix',
             executable='ros_phoenix_node',
             name='ros_phoenix',
-            parameters=[config_file],
+            parameters=[config],
             output='screen',
             respawn=False
         ),
@@ -26,7 +34,7 @@ def generate_launch_description():
             package='ros_phoenix',
             executable='falcon_motor_control_node',
             name='falcon_motor_control',
-            parameters=[config_file],
+            parameters=[config],
             output='screen',
             respawn=False
         )
