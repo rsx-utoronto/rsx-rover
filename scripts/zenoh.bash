d14a5fb9b1ad432a942d05d205cffd7203a54958
garvishb <togarvish@gmail.com>
2026-01-31 13:12:32 -0500

    zenoh configs

---
+++ scripts/zenoh.bash
@@ -0,0 +1,11 @@
+source /opt/ros/humble/setup.bash
+export RMW_IMPLEMENTATION=rmw_zenoh_cpp
+export ZENOH_ROUTER_CONFIG_URI=${HUNTER}/hunter2_zenoh.json5
+
+
+if [[ `pidof rmw_zenohd` ]];
+then
+   echo "Zenoh router already running";
+else
+   ros2 run rmw_zenoh_cpp rmw_zenohd;
+fi
